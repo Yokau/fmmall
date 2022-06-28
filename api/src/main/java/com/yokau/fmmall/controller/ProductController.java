dca7043f0d0801faca6d0a0b733f9c72198292cc
Polaris <yang722900@gmail.com>
2022-06-29 00:58:15 +0800

    商品评论信息查询接口实现

--- api/src/main/java/com/yokau/fmmall/controller/ProductController.java
+++ api/src/main/java/com/yokau/fmmall/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.yokau.fmmall.controller;
 
+import com.yokau.fmmall.service.ProductCommentsService;
 import com.yokau.fmmall.service.ProductService;
 import com.yokau.fmmall.vo.ResultVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,22 +13,40 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 @RequestMapping("/product")
-@Api(value = "提供商品信息相关的接口",tags = "商品管理")
+@Api(value = "提供商品信息相关的接口", tags = "商品管理")
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductCommentsService productCommentsService;
 
 
     @ApiOperation("商品基本信息查询接口")
     @GetMapping("/detail-info/{pid}")
-    public ResultVO getProductBasicInfo(@PathVariable("pid") String pid){
+    public ResultVO getProductBasicInfo(@PathVariable("pid") String pid) {
         return productService.getProductBasicInfo(pid);
     }
 
     @ApiOperation("商品参数信息查询接口")
     @GetMapping("/detail-params/{pid}")
-    public ResultVO getProductParams(@PathVariable("pid") String pid){
+    public ResultVO getProductParams(@PathVariable("pid") String pid) {
         return productService.getProductParamsById(pid);
+    }
+
+    @ApiOperation("商品评论信息查询接口")
+    @GetMapping("/detail-commonts/{pid}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(dataType = "int", name = "pageNum", value = "当前页码", required = true),
+            @ApiImplicitParam(dataType = "int", name = "limit", value = "每页显示条数", required = true)
+    })
+    public ResultVO getProductCommonts(@PathVariable("pid") String pid, int pageNum, int limit) {
+        return productCommentsService.listCommentsByProductId(pid, pageNum, limit);
+    }
+
+    @ApiOperation("商品评价统计查询接口")
+    @GetMapping("/detail-commontscount/{pid}")
+    public ResultVO getProductCommontsCount(@PathVariable("pid") String pid){
+        return productCommentsService.getCommentsCountByProductId(pid);
     }
 
 }
--- mapper/src/main/java/com/yokau/fmmall/dao/ProductCommentsMapper.java
+++ mapper/src/main/java/com/yokau/fmmall/dao/ProductCommentsMapper.java
@@ -1,9 +1,23 @@
 package com.yokau.fmmall.dao;
 
 import com.yokau.fmmall.entity.ProductComments;
+import com.yokau.fmmall.entity.ProductCommentsVO;
 import com.yokau.fmmall.general.GeneralDao;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductCommentsMapper extends GeneralDao<ProductComments> {
+    /**
+     * 根据商品id分页查询评论信息
+     * @param productId 商品ID
+     * @param start 起始索引
+     * @param limit 查询条数
+     * @return
+     */
+    public List<ProductCommentsVO> selectCommentsByProductId(@Param("productId") String productId,
+                                                             @Param("start") int start,
+                                                             @Param("limit") int limit);
 }
