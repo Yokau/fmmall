990c52b6de73032ca59fe3694085cece410dd03d
Polaris <yang722900@gmail.com>
2022-07-13 22:53:01 +0800

    前后端分离完成

--- service/src/main/java/com/yokau/fmmall/service/job/OrderTimeoutCheckJob.java
+++ service/src/main/java/com/yokau/fmmall/service/job/OrderTimeoutCheckJob.java
@@ -43,28 +43,30 @@ public class OrderTimeoutCheckJob {
 
         // 2. 访问微信平台接口，确认当前订单最终支付状态
         System.out.println("size : " + ordersList.size());
-        for(int i = 0; i < ordersList.size(); i++){
+        for (int i = 0; i < ordersList.size(); i++) {
             Orders order = ordersList.get(i);
             HashMap<String, String> params = new HashMap<>();
             params.put("out_trade_no", order.getOrderId());
-            System.out.println("order.getOrderId()  "+  order.getOrderId());
-            Map<String, String> resp = wxPay.orderQuery(params);
+            System.out.println("order.getOrderId()  " + order.getOrderId());
+            if (!params.isEmpty() && !order.getOrderId().isEmpty()) {
+                Map<String, String> resp = wxPay.orderQuery(params);
+                String respStatu = resp.get("trade_state");
+                if ("SUCCESS".equalsIgnoreCase(respStatu)) {
+                    //2.1 如果订单已经支付，则修改订单状态为"代发货/已支付"  status = 2
+                    Orders updateOrder = new Orders();
+                    updateOrder.setOrderId(order.getOrderId());
+                    updateOrder.setStatus("2");
+                    ordersMapper.updateByPrimaryKeySelective(updateOrder);
+                } else if ("NOTPAY".equalsIgnoreCase(respStatu)) {
+                    //2.2 如果确实未支付 则取消订单：
+                    //  a.向微信支付平台发送请求，关闭当前订单的支付链接
+                    Map<String, String> closeResponseMap = wxPay.closeOrder(params);
 
-            String respStatu = resp.get("trade_state");
-            if("SUCCESS".equalsIgnoreCase(respStatu)){
-                //2.1 如果订单已经支付，则修改订单状态为"代发货/已支付"  status = 2
-                Orders updateOrder = new Orders();
-                updateOrder.setOrderId(order.getOrderId());
-                updateOrder.setStatus("2");
-                ordersMapper.updateByPrimaryKeySelective(updateOrder);
-            } else if ("NOTPAY".equalsIgnoreCase(respStatu)) {
-                //2.2 如果确实未支付 则取消订单：
-                //  a.向微信支付平台发送请求，关闭当前订单的支付链接
-                Map<String, String> closeResponseMap = wxPay.closeOrder(params);
-
-                // 关闭订单
-                orderService.closeOrder(order.getOrderId());
+                    // 关闭订单
+                    orderService.closeOrder(order.getOrderId());
+                }
             }
+
         }
 
 
